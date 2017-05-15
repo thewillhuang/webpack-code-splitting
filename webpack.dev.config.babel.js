5296c086f16f259f262ef6e7d2dc8b88afd861c7
William Huang <will.h86@gmail.com>
2017-05-15 16:59:09 -0700

    change port

--- webpack.dev.config.babel.js
+++ webpack.dev.config.babel.js
@@ -4,7 +4,7 @@ import base, { PostCSS, CSSLoaderConfig } from './webpack.base.config.babel';
 export default Object.assign(base, {
   devServer: {
     overlay: true,
-    port: 3000,
+    port: 8000,
   },
   output: Object.assign(base.output, {
     devtoolModuleFilenameTemplate: '/[absolute-resource-path]',
