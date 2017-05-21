7cf1e8574a8b9c1cb931cefbf5d9b7be9aaf1a60
William Huang <will.h86@gmail.com>
2017-05-20 22:59:49 -0700

    switch to lerna

--- webpack.base.config.babel.js
+++ webpack.base.config.babel.js
@@ -25,11 +25,10 @@ export const CSSLoaderConfig = production => ({
 
 export default {
   entry: {
-    app1: ['./src/tools/app1'],
-    app2: ['./src/tools/app2'],
-    app3: ['./src/tools/app3'],
-    app4: ['./src/tools/app4'],
-    app5: ['./src/tools/app5'],
+    app1: ['./packages/app1'],
+    app2: ['./packages/app2'],
+    app3: ['./packages/app3'],
+    app4: ['./packages/app4'],
   },
   module: {
     rules: [
